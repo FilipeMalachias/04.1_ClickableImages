56f8ac5b073f2d141a935d325565c11c3fca0a6b
FilipeMalachias <Filipe_malachias@hotmail.com>
2019-05-06 10:41:33 -0700

    Task 3 Done

--- app/src/main/java/com/filipe/malachias/droidcafe/MainActivity.java
+++ app/src/main/java/com/filipe/malachias/droidcafe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.filipe.malachias.droidcafe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,9 +23,10 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                startActivity(intent);
             }
         });
     }
@@ -80,6 +82,4 @@ public class MainActivity extends AppCompatActivity {
     {
         displayToast(getString(R.string.froyo_order_message));
     }
-
-
 }
